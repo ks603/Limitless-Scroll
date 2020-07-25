834bd0a6ef90818ec1887137119171fb39b3bcdd
ks603 <kevin.shaughnessy18@gmail.com>
2020-07-25 10:56:41 -0500

    Foreach element in array created a link and the picture

--- script.js
+++ script.js
@@ -8,13 +8,31 @@ const count = 10
 const apiKey = 'ZE2yJ-usuSebjRqAXuFdt8tB0mbGBpfo7PY6Y1v0_yQ'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
+// Create Elements for Links and Photos
+
+function displayPhotos() {
+  photosArray.forEach((photo) => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement('a')
+    item.setAttribute('href', photo.links.html)
+    item.setAttribute('target', '_blank')
+    // Create <img> for photo
+    const img = document.createElement('img')
+    img.setAttribute('src', photo.urls.regular)
+    img.setAttribute('alt', photo.alt_description)
+    img.setAttribute('title', photo.alt_description)
+    // Put <img> inside <a>, then put both inside imageContainer element
+    item.appendChild(img)
+    imageContainer.appendChild(item)
+  })
+}
+
 // Get photos from Unspash API
 const getPhotos = async () => {
   try {
     const response = await fetch(apiUrl)
     photosArray = await response.json()
-    console.log(photosArray)
-    console.log(data)
+    displayPhotos()
   } catch (error) {}
 }
 
